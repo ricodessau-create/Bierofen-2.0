fafa82ddc88d66fc377d2b893f2711ff23e2b627
ricodessau-create <ricodessau@gmail.com>
2026-04-21 17:02:51 +0200

    Update BierOfen.java

--- src/main/java/de/bierofen/BierOfen.java
+++ src/main/java/de/bierofen/BierOfen.java
@@ -6,14 +6,19 @@ import de.bierofen.listener.FurnaceListener;
 import de.bierofen.listener.UpgradeClickListener;
 import de.bierofen.listener.AdminClickListener;
 import de.bierofen.listener.WikiClickListener;
+import de.bierofen.listener.FurnaceSmeltListener;
 import de.bierofen.storage.StorageManager;
 import de.bierofen.furnace.FurnaceManager;
+
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BierOfen extends JavaPlugin {
 
     private static BierOfen instance;
+    public static Economy econ = null;
 
     private StorageManager storageManager;
     private FurnaceManager furnaceManager;
@@ -23,6 +28,8 @@ public class BierOfen extends JavaPlugin {
         instance = this;
 
         saveDefaultConfig();
+
+        setupEconomy();
 
         storageManager = new StorageManager(getDataFolder());
         furnaceManager = new FurnaceManager(storageManager);
@@ -36,6 +43,7 @@ public class BierOfen extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new UpgradeClickListener(), this);
         Bukkit.getPluginManager().registerEvents(new AdminClickListener(), this);
         Bukkit.getPluginManager().registerEvents(new WikiClickListener(), this);
+        Bukkit.getPluginManager().registerEvents(new FurnaceSmeltListener(), this);
 
         getLogger().info("BierOfen aktiviert.");
     }
@@ -43,6 +51,24 @@ public class BierOfen extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("BierOfen deaktiviert.");
+    }
+
+    private void setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            getLogger().warning("Vault nicht gefunden! Economy deaktiviert.");
+            return;
+        }
+
+        RegisteredServiceProvider<Economy> rsp =
+                getServer().getServicesManager().getRegistration(Economy.class);
+
+        if (rsp == null) {
+            getLogger().warning("Kein Economy-Provider gefunden!");
+            return;
+        }
+
+        econ = rsp.getProvider();
+        getLogger().info("Economy erfolgreich geladen: " + econ.getName());
     }
 
     public static BierOfen getInstance() {
