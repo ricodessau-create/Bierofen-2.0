08f9ee6d7c2ac6704c37cda4118839bb794646ad
ricodessau-create <ricodessau@gmail.com>
2026-04-21 15:34:55 +0200

    Create GuiClickListener.java

---
+++ src/main/java/de/bierofen/listener/GuiClickListener.java
@@ -0,0 +1,19 @@
+package de.bierofen.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class GuiClickListener implements Listener {
+
+    @EventHandler
+    public void onClick(InventoryClickEvent e) {
+        // Standard: Alle GUIs, die keinen eigenen Listener haben, werden blockiert
+        if (e.getClickedInventory() == null) return;
+
+        // Wenn es eine GUI ist, die wir schützen wollen:
+        if (e.getView().getTitle().contains("BierOfen")) {
+            e.setCancelled(true);
+        }
+    }
+}
