f3253bb88ebdd6fc9b928d4cef6075059c90b99d
ricodessau-create <ricodessau@gmail.com>
2026-04-21 16:00:04 +0200

    Update FurnaceSmeltListener.java

--- src/main/java/de/bierofen/listener/FurnaceSmeltListener.java
+++ src/main/java/de/bierofen/listener/FurnaceSmeltListener.java
@@ -2,11 +2,12 @@ package de.bierofen.listener;
 
 import de.bierofen.BierOfen;
 import de.bierofen.furnace.FurnaceManager;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.Furnace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.FurnaceSmeltEvent;
+import org.bukkit.inventory.FurnaceInventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -16,10 +17,19 @@ public class FurnaceSmeltListener implements Listener {
     @EventHandler
     public void onSmelt(FurnaceSmeltEvent e) {
 
-        Block furnace = e.getBlock();
-        FurnaceManager fm = BierOfen.getInstance().getFurnaceManager();
+        Block block = e.getBlock();
 
-        int level = fm.getLevel(furnace);
+        // BlockState casten
+        if (!(block.getState() instanceof Furnace)) {
+            return;
+        }
+
+        Furnace furnace = (Furnace) block.getState();
+        FurnaceInventory inv = furnace.getInventory();
+
+        FurnaceManager fm = BierOfen.getInstance().getFurnaceManager();
+        int level = fm.getLevel(block);
+
         if (level <= 1) return;
 
         int chance = BierOfen.getInstance().getConfig().getInt("levels.drop." + level, 0);
@@ -35,6 +45,6 @@ public class FurnaceSmeltListener implements Listener {
         result.setAmount(extra);
 
         // Extra-Drops ins Ergebnisfach legen
-        furnace.getState().getInventory().addItem(result);
+        inv.addItem(result);
     }
 }
