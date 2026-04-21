a77cd40cad8ae76abd7efb62e42ac4327e127d27
ricodessau-create <ricodessau@gmail.com>
2026-04-21 15:26:52 +0200

    Update FurnaceListener.java

--- src/main/java/de/bierofen/listener/FurnaceListener.java
+++ src/main/java/de/bierofen/listener/FurnaceListener.java
@@ -1,65 +1,68 @@
 package de.bierofen.listener;
 
 import de.bierofen.BierOfen;
-import de.bierofen.storage.StorageManager;
+import de.bierofen.furnace.FurnaceManager;
 import de.bierofen.upgrade.FurnaceContext;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 
 public class FurnaceListener implements Listener {
-
-    @EventHandler
-    public void onPlace(BlockPlaceEvent e) {
-        Block b = e.getBlockPlaced();
-
-        if (b.getType() == Material.FURNACE
-                || b.getType() == Material.BLAST_FURNACE
-                || b.getType() == Material.SMOKER) {
-
-            StorageManager storage = BierOfen.getInstance().getStorageManager();
-            storage.setLevel(b, 1);
-
-            e.getPlayer().sendMessage(
-                    BierOfen.getInstance().getConfig().getString("messages.furnace-placed")
-                            .replace("%level%", "1")
-            );
-        }
-    }
-
-    @EventHandler
-    public void onBreak(BlockBreakEvent e) {
-        Block b = e.getBlock();
-
-        StorageManager storage = BierOfen.getInstance().getStorageManager();
-
-        if (storage.exists(b)) {
-            storage.remove(b);
-            e.getPlayer().sendMessage(
-                    BierOfen.getInstance().getConfig().getString("messages.furnace-broken")
-            );
-        }
-    }
 
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
         if (e.getClickedBlock() == null) return;
 
         Block b = e.getClickedBlock();
-        Player p = e.getPlayer();
-
         Material type = b.getType();
 
-        if (type == Material.FURNACE
-                || type == Material.BLAST_FURNACE
-                || type == Material.SMOKER) {
-
-            FurnaceContext.setLastFurnace(p, b);
+        if (type == Material.FURNACE || type == Material.BLAST_FURNACE || type == Material.SMOKER) {
+            FurnaceContext.setLastFurnace(e.getPlayer(), b);
         }
+    }
+
+    @EventHandler
+    public void onOpen(InventoryOpenEvent e) {
+        Inventory inv = e.getInventory();
+        Player p = (Player) e.getPlayer();
+
+        switch (inv.getType()) {
+            case FURNACE:
+            case BLAST_FURNACE:
+            case SMOKER:
+                break;
+            default:
+                return;
+        }
+
+        Block furnace = FurnaceContext.getLastFurnace(p);
+        if (furnace == null) return;
+
+        FurnaceManager fm = BierOfen.getInstance().getFurnaceManager();
+        int level = fm.getLevel(furnace);
+
+        String stars = generateStars(level);
+
+        InventoryView view = p.getOpenInventory();
+        view.setTitle(stars + " BierOfen");
+    }
+
+    private String generateStars(int level) {
+        if (level < 1) level = 1;
+        if (level > 5) level = 5;
+
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 1; i <= 5; i++) {
+            if (i <= level) sb.append("★");
+            else sb.append("☆");
+        }
+        sb.append("] ");
+        return sb.toString();
     }
 }
