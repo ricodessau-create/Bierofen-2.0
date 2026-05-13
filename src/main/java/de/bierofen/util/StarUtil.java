cae35e1335c41a310c8da7c432c9b2b98c24a126
ricodessau-create <ricodessau@gmail.com>
2026-05-13 10:10:58 +0200

    Refactor star display and add furnace name method
    Refactor getStars method to use max level from config and add getFurnaceName method.

--- src/main/java/de/bierofen/util/StarUtil.java
+++ src/main/java/de/bierofen/util/StarUtil.java
@@ -1,17 +1,24 @@
 package de.bierofen.util;
 
+import de.bierofen.BierOfen;
+import org.bukkit.Material;
+
 public class StarUtil {
 
     public static String getStars(int level) {
-        int max = 5;
-        int clamped = Math.max(1, Math.min(level, max));
-
-        StringBuilder sb = new StringBuilder("§6[");
-        for (int i = 1; i <= max; i++) {
-            if (i <= clamped) sb.append("★");
-            else sb.append("☆");
-        }
-        sb.append("§6]");
+        int max = BierOfen.getInstance().getConfig().getInt("settings.max-level", 5);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level;  i++) sb.append("§6★");
+        for (int i = level; i < max; i++) sb.append("§8★");
         return sb.toString();
+    }
+
+    public static String getFurnaceName(int level, Material type) {
+        String typeName = switch (type) {
+            case BLAST_FURNACE -> "Hochofen";
+            case SMOKER        -> "Räucherofen";
+            default            -> "Ofen";
+        };
+        return getStars(level) + " §e" + typeName;
     }
 }
